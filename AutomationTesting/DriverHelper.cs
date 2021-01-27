6c33a53b9634e1422ce7c83ad2b8935e0f048deb
AzucenaTAO <78100886+AzucenaTAO@users.noreply.github.com>
2021-01-27 15:50:44 -0400

    Add files via upload

---
+++ AutomationTesting/DriverHelper.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTesting
+{
+    public static class DriverHelper
+    {
+        public static void Click (By by)
+        {
+            Driver.driver.FindElement(by).Click();
+        }
+        public static void SetText(By by, string text)
+        {
+            Driver.driver.FindElement(by).SendKeys(text);
+        }
+        public static void MouseOver(By by)
+        {
+            WebDriverWait wait = new WebDriverWait(Driver.driver, TimeSpan.FromSeconds(5));
+            var element = wait.Until(ExpectedConditions.ElementIsVisible(by));
+
+            Actions action = new Actions(Driver.driver);
+            action.MoveToElement(element).Perform();
+        }
+        public static string GetText(By by)
+        {
+            return Driver.driver.FindElement(by).Text;
+        }
+    }
+}
---
+++ AutomationTesting/Login.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTesting
+{
+    public class Login
+    {
+        public void LoginUser(string email, string password)
+        {
+            DriverHelper.Click(Locators.Button.CSS_loginButton);
+            DriverHelper.SetText(Locators.Text.CSS_emailLogin, email);
+            DriverHelper.SetText(Locators.Text.CSS_passLogin,password);
+            DriverHelper.Click(Locators.Button.CSS_SignInButton);
+
+        }
+        public void GoToHome()
+        {
+            DriverHelper.Click(By.CssSelector("#header_logo a"));
+                
+
+        }
+
+    }
+}
